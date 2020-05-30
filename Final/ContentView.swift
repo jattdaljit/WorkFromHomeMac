1722af2643a725b14bfa3e6761af4ebd14f9b7c7
daljitsingh <Daljit_Singh@epam.com>
2020-05-30 16:15:36 +0530

    Initial Commit

---
+++ Final/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Final
+//
+//  Created by Daljit Singh on 30/05/20.
+//  Copyright © 2020 HackFromHome. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
