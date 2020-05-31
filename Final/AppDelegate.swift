c1d450dea03fc125786c367c11261ea7c03ddf61
daljitsingh <Daljit_Singh@epam.com>
2020-05-31 10:09:05 +0530

    working code

--- Final/AppDelegate.swift
+++ Final/AppDelegate.swift
@@ -9,115 +9,100 @@
 import Cocoa
 import SwiftUI
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var statusBarItem: NSStatusItem!
-    var active = true;
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        getActiveWindow()
-//        let statusBar = NSStatusBar.system
-//        statusBarItem = statusBar.statusItem(
-//            withLength: NSStatusItem.squareLength)
-//        statusBarItem.button?.title = "⏰"
-//
-//        let statusBarMenu = NSMenu(title: "Work From Home")
-//        statusBarItem.menu = statusBarMenu
-//
-//        statusBarMenu.addItem(
-//            withTitle: "Enable",
-//            action: #selector(AppDelegate.orderABurrito),
-//            keyEquivalent: "")
-//
-//        statusBarMenu.addItem(
-//            withTitle: "Disable",
-//            action: #selector(AppDelegate.cancelBurritoOrder),
-//            keyEquivalent: "")
-    }
-
-
-    @objc func orderABurrito() {
-        print("Enabling")
-        active = true
-        getActiveWindow()
-    }
-
-
-    @objc func cancelBurritoOrder() {
-        active = false
-    }
+@NSApplicationMain class AppDelegate : NSObject, NSApplicationDelegate {
+    var statusBarItem : NSStatusItem!
+    var active = false;
+    var timeCalculator: [String: Int] = [:]
     
-    func getActiveWindow() {
-        while(true) {
-            if(active) {
-                 temp1()
-                sleep(4)
-            } else {
-             break;
-          }
+    func applicationDidFinishLaunching (_ aNotification: Notification)
+    {
+        let statusBar = NSStatusBar.system
+        statusBarItem = statusBar.statusItem (
+            withLength: NSStatusItem.squareLength)
+        statusBarItem.button?.title = "⏰";
+        
+        let statusBarMenu = NSMenu (title: "Work From Home")
+        statusBarItem.menu = statusBarMenu
+        
+        statusBarMenu.addItem (
+            withTitle : "Enable",
+            action : #selector (AppDelegate.enableTracker),
+            keyEquivalent : ""
+        )
+        statusBarMenu.addItem (
+            withTitle : "Disable",
+            action : #selector (AppDelegate.disableTracker),
+            keyEquivalent : ""
+        )
+        statusBarMenu.addItem (
+            withTitle : "Exit",
+            action : #selector (AppDelegate.exitTracker),
+            keyEquivalent : ""
+        )
+    }
+    @objc func enableTracker()
+    {
+        getActiveWindow()
+        
+    }
+    @objc func disableTracker()
+    {
+        if(active){
+            print("Disabling")
+            active = false
+            dump(timeCalculator)
         }
     }
     
-    func temp2() {
-        let app = NSWorkspace.shared.frontmostApplication?.isActive
-        print(app)
+    @objc func exitTracker ()
+    {
+        print("Exiting app")
+        exit(0);
         
     }
     
-    func temp1() {
-        let apps = NSWorkspace.shared.runningApplications
-        for currentApp in apps.enumerated() { let runningApp = apps[currentApp.offset]; if(runningApp.activationPolicy == .regular && runningApp.isActive && runningApp.ownsMenuBar) {
-                  print(runningApp.localizedName);
-              
-                 }
-             }
-    
-    }
-    
-    func temp() {
-        let apps = NSWorkspace.shared.runningApplications
-        for currentApp in apps.enumerated() { let runningApp = apps[currentApp.offset]; if(runningApp.activationPolicy == .regular && runningApp.isActive) {
-            print(runningApp.localizedName);
+    func getActiveWindow ()
+    {
+        if (!active)
+        {
+            print ("Enabling")
+            active = true;
+            DispatchQueue.global (qos: .background).async {
+                self.temp2()
+                
             }
         }
-
-        
-        
-        let curr_app = NSWorkspace.shared.runningApplications
-        print("curr_app name")
-        print(curr_app)
-        let curr_pid = NSWorkspace.shared.frontmostApplication?.processIdentifier
-        
-//        print("curr id")
-//        print(curr_pid)
-       // let curr_app_name = curr_app?.localizedName
-        let options = CGWindowListOption.optionOnScreenOnly
-        //let windowList = CGWindowListCopyWindowInfo(options, kCGNullWindowID)
-        
-        
-        let windowListInfo = CGWindowListCopyWindowInfo(options, kCGNullWindowID);
-        let infoList = windowListInfo as NSArray? as? [[String: AnyObject]]
-//        print(infoList)
-//        print(type(of: infoList))
-        
-        
-        let infoListUnwrapped: [[String: AnyObject]] = infoList!
-    
-        
-        for info in infoListUnwrapped {
-//            print(info.values.contains(where: { (AnyObject) -> Bool in
-//                curr_pid == AnyObject
-//            }))
-            let some = info["kCGWindowOwnerPID"] as! Int32
-            if(curr_pid! == some){
-                print("final result")
-                print(info["kCGWindowOwnerName"])
-            }
-        }
-        
     }
     
+    func temp2 ()
+    {
+        while (true)
+        {
+            if (active)
+            {
+                let apps = NSWorkspace.shared.runningApplications
+                for currentApp in apps.enumerated ()
+                {
+                    let runningApp = apps [currentApp.offset];
+                    if (runningApp.activationPolicy == .regular && runningApp.isActive && runningApp.ownsMenuBar)
+                    {
+                        let keyExists = timeCalculator[runningApp.localizedName!] != nil
+                        if(keyExists){
+                            let val = timeCalculator[runningApp.localizedName!]!
+                            timeCalculator[runningApp.localizedName!] = val + 1;
+                        } else {
+                            timeCalculator[runningApp.localizedName!] = 1
+                        }
+                        print(runningApp.localizedName!);
+                    }
+                }
+                usleep (1000000)
+            }
+            else {
+                break;
+                
+            }
+        }
+    }
 }
-
-
 
