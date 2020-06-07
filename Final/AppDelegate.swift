6dca4b7d23e009a46ae360c395abba569a8c0539
daljitsingh <Daljit_Singh@epam.com>
2020-06-07 09:27:13 +0530

    added multiple file support based on date

--- Final/AppDelegate.swift
+++ Final/AppDelegate.swift
@@ -9,13 +9,20 @@
 import Cocoa
 import SwiftUI
 
-@NSApplicationMain class AppDelegate : NSObject, NSApplicationDelegate {
+@NSApplicationMain
+class AppDelegate : NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
     var statusBarItem : NSStatusItem!
     var active = false;
+    var filePath = "";
+    var folderUrl = "";
     var timeCalculator: [String: Int] = [:]
+    var notificationCounter: Int = 0;
+ 
     
     func applicationDidFinishLaunching (_ aNotification: Notification)
     {
+        
+        NSUserNotificationCenter.default.delegate = self
         let statusBar = NSStatusBar.system
         statusBarItem = statusBar.statusItem (
             withLength: NSStatusItem.squareLength)
@@ -24,22 +31,42 @@ import SwiftUI
         let statusBarMenu = NSMenu (title: "Work From Home")
         statusBarItem.menu = statusBarMenu
         
+        
         statusBarMenu.addItem (
             withTitle : "Enable",
             action : #selector (AppDelegate.enableTracker),
             keyEquivalent : ""
         )
+        
         statusBarMenu.addItem (
             withTitle : "Disable",
             action : #selector (AppDelegate.disableTracker),
             keyEquivalent : ""
         )
+        
+        statusBarMenu.addItem (
+            withTitle : "See History",
+            action : #selector (AppDelegate.seeHistory),
+            keyEquivalent : ""
+        )
+        
         statusBarMenu.addItem (
             withTitle : "Exit",
             action : #selector (AppDelegate.exitTracker),
             keyEquivalent : ""
         )
+        
+        enableTracker();
+        
     }
+    
+    @objc func seeHistory()
+    {
+        print(folderUrl)
+        NSWorkspace.shared.openFile(folderUrl, withApplication: "Finder")
+        
+    }
+    
     @objc func enableTracker()
     {
         getActiveWindow()
@@ -51,12 +78,15 @@ import SwiftUI
             print("Disabling")
             active = false
             dump(timeCalculator)
+            writeToFile()
+            showNotification()
         }
     }
     
     @objc func exitTracker ()
     {
         print("Exiting app")
+        writeToFile()
         exit(0);
         
     }
@@ -74,12 +104,18 @@ import SwiftUI
         }
     }
     
-    func temp2 ()
+    func temp2()
     {
         while (true)
         {
             if (active)
             {
+                if(notificationCounter >= 7200) {
+                    writeToFile();
+                    showNotification();
+                    notificationCounter = 0;
+                }
+                
                 let apps = NSWorkspace.shared.runningApplications
                 for currentApp in apps.enumerated ()
                 {
@@ -93,6 +129,7 @@ import SwiftUI
                         } else {
                             timeCalculator[runningApp.localizedName!] = 1
                         }
+                        notificationCounter = notificationCounter + 1;
                         print(runningApp.localizedName!);
                     }
                 }
@@ -102,6 +139,70 @@ import SwiftUI
                 break;
                 
             }
+        }
+    }
+    
+    func writeToFile() {
+        print("starting writing")
+        
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        let result = formatter.string(from: date)
+        
+        let file = "wfh-" + result + ".txt";
+
+        let text = createReadableData();
+
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            folderUrl = dir.absoluteString;
+            let fileURL = dir.appendingPathComponent(file)
+            filePath = fileURL.absoluteString;
+            do {
+                try text.write(to: fileURL, atomically: false, encoding: .utf8)
+            }
+            catch {
+            print("Unable to write to file")
+                
+            }
+        }
+        print("done writing")
+    }
+    
+    func showNotification() -> Void {
+        let notification = NSUserNotification()
+        
+        
+        notification.hasActionButton = true
+        notification.actionButtonTitle = "Report"
+        notification.title = "Work From Home"
+        notification.subtitle = "Click on report button to see details"
+        notification.soundName = NSUserNotificationDefaultSoundName
+        
+        NSUserNotificationCenter.default.deliver(notification)
+        
+        
+    }
+    
+    func createReadableData() -> String {
+        var result: String = "Time spent on each app\n";
+        for (key, value) in timeCalculator{
+            result = result +  key;
+            result = result + " : ";
+            result = result + String(value/60);
+            result = result + " minute(s)";
+            result = result + "\n";
+        }
+        print(result);
+        return result
+    }
+    
+    func userNotificationCenter(_ center: NSUserNotificationCenter, didActivate notification: NSUserNotification)  {
+        switch (notification.activationType) {
+            case .actionButtonClicked:
+                NSWorkspace.shared.openFile(filePath, withApplication: "TextEdit")
+            default:
+                break;
         }
     }
 }
